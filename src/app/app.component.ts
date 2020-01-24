76fc1f9f682cb452199e7bfd9b52dd42e217647a
Mieky Sofyan Yudinata <miki211287@gmail.com>
2020-01-24 23:34:47 +0700

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ declare const PR: any;
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements AfterViewChecked {
   public ngAfterViewChecked(): any {
